c6509ebced74cb37a58c076a5d727f7c3a63cd67
Aaron Kimbrell <akimbrell@beyondtrust.com>
2020-07-30 12:17:47 -0500

    active site stat and division of sandbars

--- app/stats.py
+++ app/stats.py
@@ -16,8 +16,20 @@ def index():
         ).data
     )
     data = {
-        'river': {
+        'sandbar_island': {
             'surveys': 0,
+            'active': 0,
+            'adult_count': 0,
+            'egg': 0,
+            'chick02': 0,
+            'chick39': 0,
+            'chick1017': 0,
+            'fledge': 0,
+            'nestingattempts': 0
+        },
+        'sandbar_main': {
+            'surveys': 0,
+            'active': 0,
             'adult_count': 0,
             'egg': 0,
             'chick02': 0,
@@ -28,6 +40,7 @@ def index():
         },
         'rooftop': {
             'surveys': 0,
+            'active': 0,
             'adult_count': 0,
             'egg': 0,
             'chick02': 0,
@@ -37,56 +50,67 @@ def index():
             'nestingattempts': 0
         }
     }
-    site_avgs = []
-    for s in range(len(sites)):
-        for i in range(len(sites[s]['surveys'])):
-            if sites[s]['loc_type'] == 'rooftop':
-                data['rooftop']['surveys'] += 1
-                data['rooftop']['chick02'] += sites[s]['surveys'][i]['chick02']
-                data['rooftop']['chick39'] += sites[s]['surveys'][i]['chick39']
-                data['rooftop']['chick1017'] += sites[s]['surveys'][i]['chick1017']
-                data['rooftop']['egg'] += sites[s]['surveys'][i]['egg1']
-                data['rooftop']['egg'] += sites[s]['surveys'][i]['egg2'] * 2
-                data['rooftop']['egg'] += sites[s]['surveys'][i]['egg3'] * 3
-                data['rooftop']['fledge'] += sites[s]['surveys'][i]['fledgling']
-            else:
-                data['river']['surveys'] += 1
-                data['river']['chick02'] += sites[s]['surveys'][i]['chick02']
-                data['river']['chick39'] += sites[s]['surveys'][i]['chick39']
-                data['river']['chick1017'] += sites[s]['surveys'][i]['chick1017']
-                data['river']['egg'] += sites[s]['surveys'][i]['egg1']
-                data['river']['egg'] += sites[s]['surveys'][i]['egg2'] * 2
-                data['river']['egg'] += sites[s]['surveys'][i]['egg3'] * 3
-                data['river']['fledge'] += sites[s]['surveys'][i]['fledgling']
-            if i == 0:
-                site_avgs.append(
-                    (
-                        sites[s]['surveys'][i]['ac1'] +
-                        sites[s]['surveys'][i]['ac2'] +
-                        sites[s]['surveys'][i]['ac3']
-                    )/3
-                )
-            elif sites[s]['surveys'][i]['site_id'] == sites[s]['surveys'][i - 1]['site_id']:
-                if sites[s - 1]['loc_type'] == 'rooftop':
-                    data['rooftop']['adult_count'] += max(site_avgs)
-                else:
-                    data['river']['adult_count'] += max(site_avgs)
-                site_avgs = []
-                site_avgs.append(
-                    (
-                        sites[s]['surveys'][i]['ac1'] +
-                        sites[s]['surveys'][i]['ac2'] +
-                        sites[s]['surveys'][i]['ac3']
-                    )/3
-                )
-            else:
-                print('else')
-                site_avgs.append(
-                    (
-                        sites[s]['surveys'][i]['ac1'] +
-                        sites[s]['surveys'][i]['ac2'] +
-                        sites[s]['surveys'][i]['ac3']
-                    )/3
-                )
+    site_avgs = [0]
+    site_count = {
+        'adult_count': 0,
+        'egg': 0,
+        'chick02': 0,
+        'chick39': 0,
+        'chick1017': 0,
+        'fledge': 0,
+    }
+    for s in range(len(sites)):  # for each site
+        for i in range(len(sites[s]['surveys'])):  # for each survey in a site
+            # simple counts per site type
+            data[sites[s]['loc_type'].replace('-', '_')]['surveys'] += 1
+            data[sites[s]['loc_type'].replace('-', '_')]['chick02'] += sites[s]['surveys'][i]['chick02']
+            data[sites[s]['loc_type'].replace('-', '_')]['chick39'] += sites[s]['surveys'][i]['chick39']
+            data[sites[s]['loc_type'].replace('-', '_')]['chick1017'] += sites[s]['surveys'][i]['chick1017']
+            data[sites[s]['loc_type'].replace('-', '_')]['egg'] += sites[s]['surveys'][i]['egg1']
+            data[sites[s]['loc_type'].replace('-', '_')]['egg'] += sites[s]['surveys'][i]['egg2'] * 2
+            data[sites[s]['loc_type'].replace('-', '_')]['egg'] += sites[s]['surveys'][i]['egg3'] * 3
+            data[sites[s]['loc_type'].replace('-', '_')]['fledge'] += sites[s]['surveys'][i]['fledgling']
+            # getting avg site data
+            site_avgs.append(
+                (
+                    sites[s]['surveys'][i]['ac1'] +
+                    sites[s]['surveys'][i]['ac2'] +
+                    sites[s]['surveys'][i]['ac3']
+                )/3
+            )
+            # counts for specific site to aid in other data
+            site_count['adult_count'] += (
+                sites[s]['surveys'][i]['ac1'] +
+                sites[s]['surveys'][i]['ac2'] +
+                sites[s]['surveys'][i]['ac3']
+            )
+            site_count['chick02'] += sites[s]['surveys'][i]['chick02']
+            site_count['chick39'] += sites[s]['surveys'][i]['chick39']
+            site_count['chick1017'] += sites[s]['surveys'][i]['chick1017']
+            site_count['egg'] += sites[s]['surveys'][i]['egg1']
+            site_count['egg'] += sites[s]['surveys'][i]['egg2'] * 2
+            site_count['egg'] += sites[s]['surveys'][i]['egg3'] * 3
+            site_count['fledge'] += sites[s]['surveys'][i]['fledgling']
+        # end iterating over surveys for site
+        data[sites[s]['loc_type'].replace('-', '_')]['adult_count'] += max(site_avgs)
+        if ((site_count['adult_count'] > 0) and (
+            (site_count['egg'] > 0) or
+            (site_count['chick02'] > 0) or
+            (site_count['chick39'] > 0) or
+            (site_count['chick1017'] > 0) or
+            (site_count['fledge'] > 0)
+        )):
+            data[sites[s]['loc_type'].replace('-', '_')]['active'] += 1
+
+        # reset for next site
+        site_count = {
+            'adult_count': 0,
+            'egg': 0,
+            'chick02': 0,
+            'chick39': 0,
+            'chick1017': 0,
+            'fledge': 0,
+        }
+        site_avgs = [0]
 
     return render_template('stats/index.jinja2', data=data)
